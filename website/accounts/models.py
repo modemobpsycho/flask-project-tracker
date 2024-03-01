16dc3ce25ab72d824ef850bd1fa6bf02b6273c20
Vadim <hawaiianwork@mail.ru>
2024-03-01 23:52:09 +0300

    create models for users

---
+++ website/accounts/models.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+
+from flask_login import UserMixin
+
+from website import bcrypt, db
+
+
+class User(UserMixin, db.Model):
+
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, unique=True, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    created_on = db.Column(db.DateTime, nullable=False)
+    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
+    confirmed_on = db.Column(db.DateTime, nullable=True)
+
+    def __init__(
+        self, email, password, is_admin=False, is_confirmed=False, confirmed_on=None
+    ):
+        self.email = email
+        self.password = bcrypt.generate_password_hash(password)
+        self.created_on = datetime.now()
+        self.is_admin = is_admin
+        self.is_confirmed = is_confirmed
+        self.confirmed_on = confirmed_on
+
+    def __repr__(self):
+        return f"<email {self.email}>"
